31d7d90de363723f097dbca728f767cbd1acdc60
Sharvari Bhosale <sharvaribhosale65@gmail.com>
2026-06-15 11:29:10 +0530

    Add files via upload

---
+++ Python/Competative/ass02_6Multiplication.py
@@ -0,0 +1,21 @@
+"""
+    Algorithm :
+        Accept first number as No1
+        Accept second number as No2
+        Perform multiplication of two numbers
+        Display result
+
+    Author  :   Sharvari Gorakhnath Bhosale
+    Date    :   15/06/2026    
+"""
+
+
+print("Enter first number : ")                          #Accept first number from user
+No1 = int(input())  
+
+print("Enter second number : ")                         #Accept second number from user
+No2 = int(input())
+
+Ans = No1 * No2                                         #Perform addition      
+
+print("Multiplication of two numbers is : ",Ans)        #Display result
---
+++ Python/Competative/ass02_9NameAge.py
@@ -0,0 +1,17 @@
+"""
+    Algorithm :
+        Accept name from user as name
+        Accept age from user as age
+        Print the required sentence
+
+    Author  :   Sharvari Gorakhnath Bhosale
+    Date    :   15/06/2026 
+"""
+
+print("Enter your name : ")                                         #Accept name from user
+name = input()
+
+print("Enter your age : ")                                          #Accept age from user
+age = int(input())
+
+print("Hello", name + ", your will turn", age+1 , "next year.")     #Print the sentence
