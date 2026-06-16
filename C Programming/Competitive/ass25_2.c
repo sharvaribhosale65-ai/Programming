0c223925986fb449ff54a7fb8f7f4da04ccc8fb9
Sharvari Bhosale <sharvaribhosale65@gmail.com>
2026-06-16 10:56:51 +0530

    Add files via upload

---
+++ C Programming/Competitive/ass25_2.c
@@ -0,0 +1,90 @@
+/*
+Algorithm:
+
+    START
+        Accept first number as iNo1
+        Accept second number as iNo2
+        Perform to display pattern
+        Display the result
+    STOP
+*/
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Include required header files
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include<stdio.h>
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Function name    :   Pattern
+// Input    :           int, int
+// Output   :           void
+// Description  :       Perform to display pattern
+// Date :               13/06/2026 
+// Author   :           Sharvari Gorakhnath Bhosale
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void Pattern(
+                int iNo1,                                   //first input       
+                int iNo2                                    //second input
+            )
+{
+    int i = 0;
+    int j = 0;
+    int iCount = 1;
+    for(i = 1; i <= iNo1; i++)
+    {
+        if(i % 2 != 0)
+        {
+            for(j = 1; j <= iNo2; j++)
+            {
+                printf("%d\t",j*2);
+            }
+        }
+        else
+        {
+           for(j = 1; j <= iNo2; j++)
+            {
+                printf("%d\t",(j*2) - 1);
+            } 
+        }
+        printf("\n");
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Application to perform to display pattern of 2 int values
+//
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+int main()
+
+{
+    int iValue1 = 0;                                       //to store first input
+    int iValue2 = 0;                                       //to store second input 
+
+    printf("Enter number of rows : ");
+    scanf("%d",&iValue1);
+ 
+    printf("Enter number of columns : ");
+    scanf("%d",&iValue2);
+
+    Pattern(iValue1, iValue2);                            //display result 
+
+    return 0;
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//Input     :   4,  5
+//Output    :   2       4       6       8       10
+//              1       3       5       7       9
+//              2       4       6       8       10
+//              1       3       5       7       9
+//
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
---
+++ C Programming/Competitive/ass28_3.c
@@ -0,0 +1,92 @@
+/*
+    Algorithm :
+        Accept first input from user as iRow
+        Accept second input from user as iCol
+        Print given pattern
+        Display result
+*/
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//Include required header files
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include<stdio.h>
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Function name    :   Pattern
+// Input    :           int, int
+// Output   :           void
+// Description  :       Perform to display pattern
+// Date :               16/06/2026 
+// Author   :           Sharvari Gorakhnath Bhosale
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void Pattern(
+                int iRow,                                         //first input
+                int iCol                                          //second input
+            )
+{
+    int i = 0;
+    int j = 0;
+
+    if(iRow != iCol)
+    {
+        printf("Invalid parameters\n");
+        printf("Rows and Columns should be same\n");
+        return;
+    }
+
+    for(i = iRow; i >= 1; i--)
+    {
+        for(j = 1; j <= iCol; j++)
+        {
+            if( i == j || i == 1 || i == iRow || j == 1 || j == iCol)
+            {
+                printf("*\t");
+            }
+            else
+            {
+                printf(" \t");
+            }
+        }
+        printf("\n");
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Application to perform to display pattern of 2 int values
+//
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+int main()
+{
+    int iValue1 = 0;                                                //to store first input
+    int iValue2 = 0;                                                //to store second input
+
+    printf("Enter number of rows : ");                              //accept rows from user
+    scanf("%d",&iValue1);
+
+    printf("Enter number of columns : ");                           //accept columns from user
+    scanf("%d",&iValue2);
+
+    Pattern(iValue1, iValue2);                                      //display result
+
+    return 0;
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//Input     :   6,  6
+//Output    :   *       *       *       *       *       *
+//              *                               *       *
+//              *                       *               *
+//              *               *                       *
+//              *       *                               *
+//              *       *       *       *       *       *
+//
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
